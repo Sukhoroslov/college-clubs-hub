9150890096cd6b302725319b4b79e1a2d427dec8
HuntersRemark <galtinmpasoglou@york.citycollege.eu>
2025-12-27 14:57:41 +0000

    Tester?

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.get('/', (req, res) => res.render('home'));
-app.listen(3000, () => console.log('Server on http://localhost:3000'));
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+
+app.use(session({
+  secret: 'your-secret-key-here',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 60 * 1000 }
+}));
+
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  res.locals.user = req.user;
+  next();
+});
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const authRoutes = require('./routes/auth');
+app.use(authRoutes);
