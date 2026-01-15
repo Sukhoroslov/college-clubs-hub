e5f889d37e98ec80c24cff8cb94ac683d9dc8911
Polychronis10 <ppolychronis@york.citycollege.eu>
2026-01-16 01:18:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
+app.get('/login', (req, res) => res.render('login'));
 app.listen(3000, () => console.log('Server on http://localhost:3000'));
